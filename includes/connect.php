20dd8599e239d61a16c5c2b5112f8a549929baf1
nikmeijer <n_meijer@fanshaweonline.ca>
2025-04-04 19:55:56 -0400

    some small updates and adjustments

--- includes/connect.php
+++ includes/connect.php
@@ -1,10 +1,9 @@
  <?php
- 
- //$connect = mysqli_connect('localhost','root','root','bookstore'); 
 
- $dsn = "mysql:host=localhost;dbname=meijer_nik_portfolio;charset=utf8mb4";
+ //$connect = mysqli_connect('localhost','root','root','bookstore'); 
+ $dsn = "mysql:host=localhost;dbname=fugbv919_nik;charset=utf8mb4";
  try {
- $connect = new PDO($dsn, 'root', '');
+ $connect = new PDO($dsn, 'fugbv919_general', 'Kolya345!');
  } catch (Exception $e) {
    error_log($e->getMessage());
    exit('unable to connect');
